45bfcf74429c752a21cedf069446111ac1ec7ff7
Oleg Yamnikov <bgeyts667@gmail.com>
2017-10-02 20:00:21 +0500

    Basic program declaration parser

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,17 +1,31 @@
 module Main where
 
 import qualified Data.Text.IO       as TIO
-import           Pilya.Lex
+import qualified Pilya.Lex          as Lex
+import qualified Pilya.Syn          as Syn
 import           System.Environment (getArgs)
 
 lexParseAndPrint :: String -> IO ()
 lexParseAndPrint path = do
     text <- TIO.readFile path
-    case parse text of
-        (Left (ParserError line pos msg)) ->
+    case Lex.parse text of
+        (Left (Lex.ParserError line pos msg)) ->
             putStrLn (show line ++ ":" ++ show pos ++ ": " ++ msg)
         (Right tokens) ->
             mapM_ print tokens
+
+synParseAndPrint :: String -> IO ()
+synParseAndPrint path = do
+    text <- TIO.readFile path
+    case Lex.parse text of
+        Left (Lex.ParserError line pos msg) ->
+            putStrLn (show line ++ ":" ++ show pos ++ ": " ++ msg)
+        Right tokens ->
+            case Syn.parse tokens of
+                Left (Syn.ParserError msg line pos) ->
+                    putStrLn (show line ++ ":" ++ show pos ++ ": " ++ show msg)
+                Right prg ->
+                    print prg
 
 printUsage :: IO ()
 printUsage = do
@@ -26,4 +40,5 @@ main = do
     args <- getArgs
     case args of
         ["lex", path] -> lexParseAndPrint path
+        ["syn", path] -> synParseAndPrint path
         _             -> printUsage
--- src/Pilya/Syn.hs
+++ src/Pilya/Syn.hs
@@ -1,18 +1,63 @@
 module Pilya.Syn
     ( parse
+    , ParserError (..)
     )
     where
 
 import           Pilya.Lex     (Token (..), TokenType (..))
-import           Pilya.Parcomb (Parser (..), ParserError (..), consume)
+import           Pilya.Parcomb (Parser (..), ParserError (..), consume, expect,
+                                expectAny, lookahead, parserError)
 import qualified Pilya.Parcomb as Parcomb
 
-testParse :: Parser (TokenType, TokenType, TokenType)
-testParse = do
-    t1 <- consume
-    t2 <- consume
-    t3 <- consume
-    return (t1, t2, t3)
+type Identifier = String
 
-parse :: [Token] -> Either ParserError (TokenType, TokenType, TokenType)
-parse = Parcomb.parse testParse
+data Type
+    = TypeInt
+    | TypeReal
+    | TypeBool
+    deriving (Show)
+
+typeFromToken :: TokenType -> Type
+typeFromToken TokPercent = TypeInt
+typeFromToken TokExcl = TypeReal
+typeFromToken TokDollar = TypeBool
+typeFromToken tt = error $ "Token " ++ show tt ++ " does not denote a type"
+
+data Declaration = Declaration [Identifier] Type
+    deriving (Show)
+
+identifier :: Parser Identifier
+identifier = do
+    tt <- lookahead
+    case tt of
+        TokIdent s -> do { consume; return s }
+        _          -> parserError "Expected identifier"
+
+declaration :: Parser Declaration
+declaration = do
+    expect TokKwDim
+    ident <- identifier
+    typeTok <- expectAny [TokPercent, TokExcl, TokDollar]
+    return $ Declaration [ident] (typeFromToken typeTok)
+
+data Block
+    = BlockDecl Declaration
+    | BlockStmt
+    deriving (Show)
+
+block :: Parser Block
+block = do
+    decl <- declaration
+    return $ BlockDecl decl
+
+newtype Program = Program [Block]
+    deriving (Show)
+
+program :: Parser Program
+program = do
+    b <- block
+    expect TokKwEnd
+    return $ Program [b]
+
+parse :: [Token] -> Either ParserError Program
+parse = Parcomb.parse program
